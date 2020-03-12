ef92b962de69fd96403bf78271af97cd7ed6e2ea
andy23512 <andy23512@gmail.com>
2020-03-12 22:53:37 +0800

    Use typescript

---
+++ dist/ctore.js
@@ -0,0 +1,86 @@
+#!/usr/bin/env node
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var child_process_1 = __importDefault(require("child_process"));
+var command_exists_1 = __importDefault(require("command-exists"));
+var inquirer_1 = __importDefault(require("inquirer"));
+var projectName = '';
+var userOptions;
+getProjectName()
+    .then(function () { return command_exists_1.default('ng'); })
+    .then(function () { return command_exists_1.default('create-nx-workspace'); })
+    .then(getConfig)
+    .then(runCli)
+    .then(changeDirToProject)
+    .then(addSchematics)
+    // tslint:disable-next-line: no-console
+    .catch(console.error);
+function getProjectName() {
+    return new Promise(function (resolve, reject) {
+        projectName = process.argv[2];
+        projectName
+            ? resolve()
+            : reject(new Error('Error: No project name is given\nUsage: ctore <project-name>'));
+    });
+}
+function getConfig() {
+    // tslint:disable-next-line: no-console
+    console.clear();
+    return inquirer_1.default
+        .prompt([
+        {
+            type: 'checkbox',
+            name: 'schematics',
+            message: 'Which schematics do you want to add?',
+            choices: [
+                {
+                    name: 'Angular Material (@angular/material)',
+                    value: '@angular/material',
+                    short: 'Angular Material'
+                },
+                // {name: 'Jest (@briebug/jest-schematic)', value: '@briebug/jest-schematic', short: 'Jest'},
+                {
+                    name: 'NGXS (@ngxs/schematics)',
+                    value: '@ngxs/schematics',
+                    short: 'NGXS'
+                },
+                {
+                    name: 'Apollo Angular (apollo-angular)',
+                    value: 'apollo-angular',
+                    short: 'Apollo Angular'
+                }
+            ]
+        }
+    ])
+        .then(function (answers) {
+        userOptions = answers;
+    });
+}
+function runCli() {
+    return promiseSpawn('create-nx-workspace', [
+        projectName,
+        '--preset=angular',
+        '--appName=app',
+        '--style=scss'
+    ]).catch(function (code) {
+        throw new Error('create-nx-workspace exited with error code ' + code);
+    });
+}
+function changeDirToProject() {
+    return Promise.resolve(process.chdir(projectName));
+}
+function addSchematics() {
+    return userOptions.schematics.reduce(function (p, schematic) {
+        return p.then(function (_) { return promiseSpawn('ng', ['add', schematic]); });
+    }, Promise.resolve());
+}
+function promiseSpawn(command, args) {
+    return new Promise(function (resolve, reject) {
+        child_process_1.default
+            .spawn(command, args, { shell: true, stdio: 'inherit' })
+            .on('close', function (code) { return (code === 0 ? resolve() : reject()); });
+    });
+}
---
+++ src/ctore.ts
@@ -0,0 +1,97 @@
+#!/usr/bin/env node
+import childProcess from 'child_process';
+import commandExists from 'command-exists';
+import inquirer from 'inquirer';
+
+let projectName = '';
+let userOptions: {
+  schematics: any;
+};
+
+getProjectName()
+  .then(() => commandExists('ng'))
+  .then(() => commandExists('create-nx-workspace'))
+  .then(getConfig)
+  .then(runCli)
+  .then(changeDirToProject)
+  .then(addSchematics)
+  // tslint:disable-next-line: no-console
+  .catch(console.error);
+
+function getProjectName() {
+  return new Promise((resolve, reject) => {
+    projectName = process.argv[2];
+    projectName
+      ? resolve()
+      : reject(
+          new Error(
+            'Error: No project name is given\nUsage: ctore <project-name>'
+          )
+        );
+  });
+}
+
+function getConfig() {
+  // tslint:disable-next-line: no-console
+  console.clear();
+  return inquirer
+    .prompt([
+      {
+        type: 'checkbox',
+        name: 'schematics',
+        message: 'Which schematics do you want to add?',
+        choices: [
+          {
+            name: 'Angular Material (@angular/material)',
+            value: '@angular/material',
+            short: 'Angular Material'
+          },
+          // {name: 'Jest (@briebug/jest-schematic)', value: '@briebug/jest-schematic', short: 'Jest'},
+          {
+            name: 'NGXS (@ngxs/schematics)',
+            value: '@ngxs/schematics',
+            short: 'NGXS'
+          },
+          {
+            name: 'Apollo Angular (apollo-angular)',
+            value: 'apollo-angular',
+            short: 'Apollo Angular'
+          }
+        ]
+      }
+    ])
+    .then(answers => {
+      userOptions = answers;
+    });
+}
+
+function runCli() {
+  return promiseSpawn('create-nx-workspace', [
+    projectName,
+    '--preset=angular',
+    '--appName=app',
+    '--style=scss'
+  ]).catch(code => {
+    throw new Error('create-nx-workspace exited with error code ' + code);
+  });
+}
+
+function changeDirToProject() {
+  return Promise.resolve(process.chdir(projectName));
+}
+
+function addSchematics() {
+  return userOptions.schematics.reduce(
+    (p: Promise<any>, schematic: string) =>
+      p.then(_ => promiseSpawn('ng', ['add', schematic])),
+    Promise.resolve()
+  );
+}
+
+function promiseSpawn(command: string, args: string[]) {
+  return new Promise((resolve, reject) => {
+    childProcess
+      .spawn(command, args, { shell: true, stdio: 'inherit' })
+      .on('close', code => (code === 0 ? resolve() : reject()));
+  });
+}
